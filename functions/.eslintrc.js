cc467df20cf722ab1471992afa1a220c190d7049
Raven Jiang <raven@cs.stanford.edu>
2021-10-09 16:23:30 -0700

    Update hosting deployment

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,13 +19,25 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
+    ".eslintrc.js",
+    "/src/generated/**/*",
+    "babel.config.js"
   ],
   plugins: [
     "@typescript-eslint",
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
+    quotes: ["error", "single"],
+    indent: 0,
+    "object-curly-spacing": 0,
+    "require-jsdoc": 0,
+    "max-len": 0,
+    "@typescript-eslint/no-namespace": 0,
+    "@typescript-eslint/no-empty-interface": 0,
+    "space-before-function-paren": 0,
+    "operator-linebreak": 0,
+    "new-cap": 0,
+    "camelcase": 0,
   },
 };
