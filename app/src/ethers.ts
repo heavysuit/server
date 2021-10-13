a5233ec67ff4fb546c72f9795b9ec168f3545ade
Raven Jiang <raven@cs.stanford.edu>
2021-10-13 11:50:59 -0700

    Remove app directory

--- app/src/ethers.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from 'ethers';
-
-export const metamask = (window as any).ethereum;
-export const provider = new ethers.providers.Web3Provider(metamask);
--- app/src/store.ts
+++
@@ -1,6 +0,0 @@
-import { Drizzle } from '@drizzle/store';
-import HeavySuit from './contracts/HeavySuit.json';
-
-export const drizzle = new Drizzle({
-  contracts: [HeavySuit as any],
-});
