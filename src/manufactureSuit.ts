1bb71c25613d569a252397224a8ebf6e1cd7eb93
Raven Jiang <raven@cs.stanford.edu>
2021-10-14 22:03:44 -0700

    Firestore whitelist

--- src/manufactureSuit.ts
+++ src/manufactureSuit.ts
@@ -2,9 +2,11 @@ import { Storage } from '@google-cloud/storage';
 import { ethers } from 'ethers';
 import * as functions from 'firebase-functions';
 import { createMetadata } from './createMetadata';
+import { firebase } from './firebase';
 import { hs } from './HeavySuit';
 import { logger } from './utils/logger';
 
+const db = firebase.firestore();
 const storage = new Storage();
 
 export const manufactureSuit = functions
@@ -25,6 +27,12 @@ export const manufactureSuit = functions
       deposits: ethers.utils.formatEther(deposits),
     });
 
+    const doc = await db.collection('wallets').doc(address).get();
+    const walletInfo = doc.data();
+    if (!walletInfo?.whitelisted) {
+      throw new Error('Your address is not allowed to mint');
+    }
+
     if (deposits.lt(fee)) {
       throw new Error('Insufficient deposits');
     }
@@ -34,12 +42,14 @@ export const manufactureSuit = functions
     }
 
     const mintTx = await hs.manufacture(address);
+    logger.info('Submitting transaction', { tx: mintTx });
     await mintTx.wait();
 
     const tokenBalance = await hs.balanceOf(address);
     const tokenId = await hs.tokenOfOwnerByIndex(address, tokenBalance.sub(1));
 
     const metadata = await createMetadata(name, tokenId.toString());
+    logger.info('Uploading metadata', { tokenId, metadata });
 
     const bucketName = 'hs-metadata';
     const bucket = storage.bucket(bucketName);
