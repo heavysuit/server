975b17df4e6995b6ccd2b9ece967256371333104
Raven Jiang <raven@cs.stanford.edu>
2021-10-15 15:06:30 -0700

    CORS for manufactureSuit

--- src/functions/manufactureSuit.ts
+++ src/functions/manufactureSuit.ts
@@ -1,6 +1,7 @@
 import { Storage } from '@google-cloud/storage';
 import { ethers } from 'ethers';
 import * as functions from 'firebase-functions';
+import { cors } from '../cors';
 import { createMetadata } from '../createMetadata';
 import { firebase } from '../firebase';
 import { hs } from '../HeavySuit';
@@ -15,55 +16,66 @@ export const manufactureSuit = functions
     memory: '4GB',
   })
   .https.onRequest(async (request, response) => {
-    const { address, name } = request.body as {
-      address: string;
-      name: string;
-    };
-    const fee = await hs.orderFee();
-    const deposits = await hs.depositOf(address);
-    logger.info(address, {
-      address,
-      fee: ethers.utils.formatEther(fee),
-      deposits: ethers.utils.formatEther(deposits),
+    cors(request, response, async () => {
+      const { address, name } = request.body as {
+        address: string;
+        name: string;
+      };
+      const fee = await hs.orderFee();
+      const deposits = await hs.depositOf(address);
+      logger.info(address, {
+        address,
+        fee: ethers.utils.formatEther(fee),
+        deposits: ethers.utils.formatEther(deposits),
+      });
+
+      const doc = await db.collection('wallets').doc(address).get();
+      const walletInfo = doc.data();
+      if (!walletInfo?.whitelisted) {
+        response.status(403);
+        response.send('Your address is not allowed to mint');
+        return;
+      }
+
+      if (deposits.lt(fee)) {
+        response.status(402);
+        response.send('Insufficient deposits');
+        return;
+      }
+
+      if (!name) {
+        response.status(400);
+        response.send('Missing name');
+        return;
+      }
+
+      const mintTx = await hs.manufacture(address);
+      logger.info('Submitting transaction', { tx: mintTx });
+      await mintTx.wait();
+
+      const tokenBalance = await hs.balanceOf(address);
+      const tokenId = await hs.tokenOfOwnerByIndex(
+        address,
+        tokenBalance.sub(1),
+      );
+
+      const metadata = await createMetadata(name, tokenId.toString());
+      logger.info('Uploading metadata', { tokenId, metadata });
+
+      const bucketName = 'hs-metadata';
+      const bucket = storage.bucket(bucketName);
+
+      const blob = bucket.file(`versions/${tokenId}.json`);
+      const blobStream = blob.createWriteStream();
+
+      const promise = new Promise((resolve, reject) => {
+        blobStream.on('error', reject);
+        blobStream.on('finish', () => resolve(true));
+        blobStream.end(Buffer.from(JSON.stringify(metadata)));
+      });
+
+      await promise;
+
+      response.json({ tokenId: tokenId.toNumber() });
     });
-
-    const doc = await db.collection('wallets').doc(address).get();
-    const walletInfo = doc.data();
-    if (!walletInfo?.whitelisted) {
-      throw new Error('Your address is not allowed to mint');
-    }
-
-    if (deposits.lt(fee)) {
-      throw new Error('Insufficient deposits');
-    }
-
-    if (!name) {
-      throw new Error('Missing name');
-    }
-
-    const mintTx = await hs.manufacture(address);
-    logger.info('Submitting transaction', { tx: mintTx });
-    await mintTx.wait();
-
-    const tokenBalance = await hs.balanceOf(address);
-    const tokenId = await hs.tokenOfOwnerByIndex(address, tokenBalance.sub(1));
-
-    const metadata = await createMetadata(name, tokenId.toString());
-    logger.info('Uploading metadata', { tokenId, metadata });
-
-    const bucketName = 'hs-metadata';
-    const bucket = storage.bucket(bucketName);
-
-    const blob = bucket.file(`versions/${tokenId}.json`);
-    const blobStream = blob.createWriteStream();
-
-    const promise = new Promise((resolve, reject) => {
-      blobStream.on('error', reject);
-      blobStream.on('finish', () => resolve(true));
-      blobStream.end(Buffer.from(JSON.stringify(metadata)));
-    });
-
-    await promise;
-
-    response.json({ tokenId: tokenId.toNumber() });
   });
