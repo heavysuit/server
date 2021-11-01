ebe2b8f3ef4559e63a888ac4fec47159ed4cc602
Raven Jiang <raven@cs.stanford.edu>
2021-10-31 19:06:45 -0700

    Remove minting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { getOrders } from './functions/getOrders';
 import { joinWaitlist } from './functions/joinWaitlist';
-import { manufactureSuit } from './functions/manufactureSuit';
 
-export { manufactureSuit, getOrders, joinWaitlist };
+export { joinWaitlist };
