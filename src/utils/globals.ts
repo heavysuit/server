7c398b2dc162ae27cf29c2def56f1e9f0a0c4e8c
Raven Jiang <raven@cs.stanford.edu>
2021-11-23 15:04:16 -0800

    Unit-02

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const PAINT_COLOR = 'Urban Tactical Camo';
-export const PAINT_DIR = 'jc6';
+export const PAINT_COLOR = 'Unit-02';
+export const PAINT_DIR = 'unit02';
 export const BATCH = 'delta';
