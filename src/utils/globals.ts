324ba4144e1598b139169787d49a57ac6d0fab45
Raven Jiang <raven@cs.stanford.edu>
2021-11-23 22:39:08 -0800

    Gradient 2

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const PAINT_COLOR = 'Unit-02';
-export const PAINT_DIR = 'unit02';
+export const PAINT_COLOR = 'Halloween Dusk';
+export const PAINT_DIR = 'gdr2';
 export const BATCH = 'delta';
