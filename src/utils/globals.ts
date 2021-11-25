f03cfc2232655bbec585330cfa8aa348fc128d72
Raven Jiang <raven@cs.stanford.edu>
2021-11-25 12:02:03 -0800

    Unit-00

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const PAINT_COLOR = 'Ayanami Blue';
-export const PAINT_DIR = 'unit00';
+export const PAINT_COLOR = 'Unit-00';
+export const PAINT_DIR = 'unit00-2';
 export const BATCH = 'delta';
